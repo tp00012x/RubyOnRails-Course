1b4fb6c5a0148fce67eeb58105e0f12476fbfcc6
Anthony Torres <anthony_torres01@yahoo.com>
2017-10-26 09:54:19 -0500

    Make app production ready

--- Rails/rails_projects/test_app/config/routes.rb
+++ Rails/rails_projects/test_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome/home', root: 'welcome#home'
-  get 'welcome/about', root: 'welcome#about'
+  root 'welcome#home'
+  get 'about', to: 'welcome#about'
 end
